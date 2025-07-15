a811724fbc8278363087d1866af8d7462a3447a5
roniwellington <roniwellington@gmail.com>
2025-07-15 12:45:55 -0300

    criando o categorizador de Produtos

---
+++ src/main/java/br/com/alura/ecommerce/CategorizadorDeProdutos.java
@@ -0,0 +1,34 @@
+package br.com.alura.ecommerce;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.util.Arrays;
+
+public class CategorizadorDeProdutos {
+    public static void main(String[] args) {
+        var user = "Escova de dentes";
+        var system = "Você é um categorizador de produtos";
+
+        var chave = System.getenv("OPENAI_KEY");
+        var service = new OpenAiService(chave);
+
+        var completionRequest = ChatCompletionRequest
+                .builder()
+                .model("babbage-002")
+                .messages(Arrays.asList(
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(),system)
+                ))
+                .n(5)
+                .build();
+        service.createChatCompletion(completionRequest)
+                .getChoices()
+                .forEach(c ->{
+                        System.out.print(c.getMessage().getContent());
+                    System.out.println("----------------------------");
+                });
+    }
+}
