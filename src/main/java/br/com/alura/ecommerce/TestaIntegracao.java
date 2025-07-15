22310ced47fdf1a9e874f3894e0277e7728e190b
roniwellington <roniwellington@gmail.com>
2025-07-15 12:27:29 -0300

    adicionando a chave da API

--- src/main/java/br/com/alura/ecommerce/TestaIntegracao.java
+++ src/main/java/br/com/alura/ecommerce/TestaIntegracao.java
@@ -1,2 +1,31 @@
-package br.com.alura.ecommerce;public class TestaIntegracao {
+package br.com.alura.ecommerce;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.util.Arrays;
+
+public class TestaIntegracao {
+    public static void main(String[] args) {
+        var user = "Gere 5 produtos";
+        var system = "Você é um gerador de produtos fictícios para um ecommerce e deve gerar apenas o nome dos produtos solicitados pelo usuário";
+
+        var chave = System.getenv("OPENAI_KEY");
+        var service = new OpenAiService(chave);
+
+        var completionRequest = ChatCompletionRequest
+                .builder()
+                .model("babbage-002")
+                .messages(Arrays.asList(
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(),system)
+                ))
+                .build();
+        service.createChatCompletion(completionRequest)
+                .getChoices()
+                .forEach(c -> System.out.print(c.getMessage().getContent()));
+    }
 }
